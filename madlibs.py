a7fe0f9b1d75399fa8b0d6bc3f8f30e719b0cc9b
Leeza Huor <leezahuor@Leezas-MacBook-Air.local>
2022-10-03 11:41:08 -0700

    Added inputs.

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#inputs
+noun = input("Noun: ")
+noun2 = input("Another noun: ")
+adj = input("Adjective: ")
+verb = input("Verb: ")
+
+#madlib
+print(noun + " is brown.")
+print(noun2 + " is white.")
+print("They both are " + adj + ".")
+print("They like to " + verb + ".")
---
+++ madlibs2.py
@@ -0,0 +1,9 @@
+noun1 = input("Noun: ")
+noun2 = input("Another noun: ")
+adj = input("Adjective: ")
+verb = input("Verb: ")
+
+madlib = f"{noun1} likes to sleep. {noun2} likes to eat. \
+They are both {adj}. They enjoy {verb} together."
+
+print(madlib) 
